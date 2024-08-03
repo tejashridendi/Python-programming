1da00fa8bff08a4d7e984b82b58d38d1cc015f27
tejashridendi <106474690+tejashridendi@users.noreply.github.com>
2024-08-04 00:01:43 +0530

    Delete NoteTakingAppSQLQueries.sql

--- NoteTakingAppSQLQueries.sql
+++
@@ -1,6 +0,0 @@
---created a database named 'notespro' for the note taking desktop application
-create database notespro;
---created a table 'notes' in database named 'notespro' for the note taking desktop application with columns - ID for note id, text for note text and status column for marking as completed/pending
-create table notespro.notes(
-id INT AUTO_INCREMENT NOT NULL UNIQUE, text VARCHAR(255) NOT NULL,status ENUM('pending', 'completed') DEFAULT 'pending'
-);
